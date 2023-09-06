c35697333f4451ec9aca86078ea570a56e9ec2b0
AlessandroPerelli <alessandroperelli156@gmail.com>
2023-09-06 18:29:13 +0100

    data points are now caped at 1000

--- src/App.tsx
+++ src/App.tsx
@@ -38,19 +38,24 @@ class App extends Component<{}, IState> {
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // If array is empty, return
-      if (!serverResponds.length) {
-        return;
-      }
+    let x = 0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        // If array is empty, return
+        if (!serverResponds.length) {
+          return;
+        }
 
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: serverResponds, showGraph: true }, () => {
-        // Call getDataFromServer() again after 100ms
-        setTimeout(() => this.getDataFromServer(), 100);
+        // Update the state by creating a new array of data that consists of
+        // Previous data in the state and the new data from server
+        this.setState({ data: serverResponds, showGraph: true })
       });
-    });
+      x++;
+      console.log(x)
+      if (x > 1000) {
+        clearInterval(interval);
+      }
+    })
   }
 
   /**
